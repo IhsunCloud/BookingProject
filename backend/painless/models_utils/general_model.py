1066795e3f369c4ffe7598041cd19b5d79378388
ihsunmozdeh <ihsun@tuta.io>
2022-12-05 22:24:01 +0330

    Add general_model which contains fields to inherit.

---
+++ backend/painless/models_utils/general_model.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.templatetags import slugify
+from django.utils.translation import gettext_lazy as _
+
+
+class GeneralModel(models.Model):
+    """
+    Model definition of General Model.
+    """
+    title = models.CharField(
+        _('Title'),
+        max_length = 128,
+    )
+    slug  = models.SlugField(
+        _('Slug'),
+        unique = True,
+        max_length = 128,
+        allow_unicode = True,
+    )
+    
+    class Meta:
+        """
+        Meta information of General Model.
+        """
+        abstract = True
+        verbose_name = _('General Model')
+        verbose_name_plural = _('General Models')
+    
+    def save(self, *args, **kwargs):
+        """
+        Slugify the title.
+        """
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(GeneralModel, self).save(*args, **kwargs)
