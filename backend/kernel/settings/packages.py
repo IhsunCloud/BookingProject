bcbe9edb0501acbae3cddc4648e20cb851f9c1a6
ihsunmozdeh <ihsun@tuta.io>
2022-12-05 22:17:36 +0330

    🚀 Madular Django settings.

---
+++ backend/kernel/settings/packages.py
@@ -0,0 +1,9 @@
+from .base import INSTALLED_APPS
+
+# Custom Project
+INSTALLED_APPS.append('booking')
+INSTALLED_APPS.append('cart')
+INSTALLED_APPS.append('user')
+
+# Python Packages
+INSTALLED_APPS.append('rest_framework')
