089645fafa2120690073f310634dbfb0046009c2
ihsunmozdeh <ihsun@tuta.io>
2022-12-09 02:07:34 +0330

    🚀 Secure DjangoAdmin with OTP.

--- backend/kernel/settings/packages.py
+++ backend/kernel/settings/packages.py
@@ -5,5 +5,7 @@ INSTALLED_APPS.append('booking')
 INSTALLED_APPS.append('cart')
 INSTALLED_APPS.append('user')
 
-# Python Packages
+# 3rd-Party Packages
+INSTALLED_APPS.append('django_otp',)
+INSTALLED_APPS.append('django_otp.plugins.otp_totp')
 INSTALLED_APPS.append('rest_framework')
--- backend/kernel/urls.py
+++ backend/kernel/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django_otp.admin import OTPAdminSite
+
+# Secure Django Admin
+# admin.site.__class__ = OTPAdminSite
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
