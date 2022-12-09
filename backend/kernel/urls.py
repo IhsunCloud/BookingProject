67ebee3abb0373c5dacdb3edd00dac190b756220
ihsunmozdeh <ihsun@tuta.io>
2022-12-09 04:11:44 +0330

    🚀 Add api urls.

--- backend/kernel/urls.py
+++ backend/kernel/urls.py
@@ -13,16 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django_otp.admin import OTPAdminSite
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from django_otp.admin import OTPAdminSite
 
 # Secure Django Admin
 # admin.site.__class__ = OTPAdminSite
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.v1.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
