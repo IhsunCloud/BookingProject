253cf56e79c6cd050cfcc321e557852f02be2d0e
ihsunmozdeh <ihsun@tuta.io>
2022-12-10 18:50:10 +0330

    🚀 Del TimeStampedModel.

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from painless.models import TimeStampedModel
 
-
-class Order(TimeStampedModel):
+class Order(models.Model):
 	"""
  	Model definition for Orders.
   	"""
