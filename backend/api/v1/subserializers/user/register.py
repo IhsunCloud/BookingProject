4f72fd9364f08ebe168453d08c94901fd83d2c47
ihsunmozdeh <ihsun@tuta.io>
2022-12-10 18:24:03 +0330

    🚀 Add register serializer.

---
+++ backend/api/v1/subserializers/user/register.py
@@ -0,0 +1,58 @@
+import random
+import pyotp
+
+from rest_framework import serializers
+
+from user.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+	class Meta:
+		model  = User
+		fields = (
+	  		'mobile',
+		)
+
+	def create(self, validated_data):
+		
+		instance = self.Meta.model(**validated_data)
+  
+		global totp
+		totp = pyotp.TOTP('base32secret3232')
+		totp.now() # e.g.: `492039`
+		secret = pyotp.random_base32()
+		totp   = pyotp.TOTP(secret, interval=300)
+		otp    = totp.now()
+  
+		instance = self.Meta.model.objects.update_or_create(
+	  		**validated_data,
+			defaults = dict(
+				otp  = str(random.randint(1000 , 9999)
+			)))[0]
+ 
+		instance.save()
+		return instance
+
+
+class VerifyOTPSerializer(serializers.ModelSerializer):
+		class Meta:
+			model  = User
+			fields = ['mobile','otp']
+			
+		def create(self,validated_data):
+			instance = self.Meta.model(**validated_data)
+   
+			keywords = "123456789"
+			result   = "@" + str(''.join(
+	   			random.choices(keywords, k=6)
+		  	))
+   
+			instance = self.Meta.model.objects.update_or_create(
+	   			**validated_data,
+		  		defaults = dict(
+					username = result,
+				 	name = instance.phone_number,
+				))[0]
+   
+			instance.save()
+			return instance
--- backend/painless/models_utils/choices.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-
-class STATUS(models.TextChoices):
-    DRAFT     = 'D', _('Draft'),
-    PUBLISHED = 'P', _('Published'),
