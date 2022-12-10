899b2f89253e5ef44f2f05ae7d857e711041385c
ihsunmozdeh <ihsun@tuta.io>
2022-12-10 18:24:17 +0330

    🚀 Add register serializer.

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,1 +1,2 @@
-from api.v1.subserializers.register import RegisterSerializer
+from api.v1.subserializers.user.register import RegisterSerializer
+from api.v1.subserializers.user.register import VerifyOTPSerializer
