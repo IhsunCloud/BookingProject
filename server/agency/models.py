f012d1e6a02b07484bc6f8b77644c1c36d0e8621
ihsunmozdeh <ihsun@tuta.io>
2022-12-15 07:59:29 +0330

    🚀 Add agency base model.

--- server/agency/models.py
+++ server/agency/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from painless.models import SluggedModel, TimeStampedModel
+
+class Agency(SluggedModel, TimeStampedModel):
+    pass
