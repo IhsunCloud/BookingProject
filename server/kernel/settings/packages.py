6cd50f034a05af204d61a801bb56fb852d6b1c2d
ihsunmozdeh <ihsun@tuta.io>
2022-12-20 07:31:18 +0330

    🚀 del extra comments

--- server/kernel/settings/packages.py
+++ server/kernel/settings/packages.py
@@ -1,4 +1,6 @@
+# import pyjion
 from .base import INSTALLED_APPS
+
 
 # Custom Project
 INSTALLED_APPS.append('agency')
@@ -15,7 +17,10 @@ INSTALLED_APPS.append('django_otp.plugins.otp_totp')
 INSTALLED_APPS.append('djoser')
 INSTALLED_APPS.append('model_utils')
 INSTALLED_APPS.append('phonenumbers')
+# INSTALLED_APPS.append('pyjion')
 INSTALLED_APPS.append('rest_framework')
 INSTALLED_APPS.append('rest_framework.authtoken')
 
 CART_SESSION_ID = 'cart'
+
+# pyjion.set_optimization_level(0)
